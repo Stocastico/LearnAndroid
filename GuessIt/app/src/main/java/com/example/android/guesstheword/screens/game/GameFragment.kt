914e2497fbf79eef12bd6d4b45576027c3d1320d
Stefano Masneri <smasneri@vicomtech.org>
2019-06-03 13:57:52 +0200

    Complete GuessIt project

--- GuessIt/app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
+++ GuessIt/app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
@@ -16,13 +16,18 @@
 
 package com.example.android.guesstheword.screens.game
 
+import android.content.Context.VIBRATOR_SERVICE
+import android.os.Build
 import android.os.Bundle
-import android.util.Log
+import android.os.VibrationEffect
+import android.os.Vibrator
+import android.text.format.DateUtils
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.fragment.NavHostFragment.findNavController
 import com.example.android.guesstheword.R
@@ -35,15 +40,6 @@ class GameFragment : Fragment() {
 
     // viewModel
     private lateinit var viewModel : GameViewModel
-
-    // The current word
-    private var word = ""
-
-    // The current score
-    private var score = 0
-
-    // The list of words - the front of the list is the next word to guess
-    private lateinit var wordList: MutableList<String>
 
     private lateinit var binding: GameFragmentBinding
 
@@ -58,92 +54,48 @@ class GameFragment : Fragment() {
                 false
         )
 
-        Log.i("GameFragment", "Calling ViewModelProviders.of")
         viewModel = ViewModelProviders.of(this).get(GameViewModel::class.java)
 
-        resetList()
-        nextWord()
+        binding.gameViewModel = viewModel
+        binding.setLifecycleOwner(this)
 
-        binding.correctButton.setOnClickListener { onCorrect() }
-        binding.skipButton.setOnClickListener { onSkip() }
-        updateScoreText()
-        updateWordText()
+        viewModel.eventGameFinish.observe(this, Observer { hasFinished ->
+            if (hasFinished) {
+                gameFinished()
+                viewModel.onGameFinishComplete()
+            }
+        })
+
+        viewModel.eventBuzz.observe(this, Observer {buzzType ->
+            if (buzzType != GameViewModel.BuzzType.NO_BUZZ) {
+                buzz(buzzType.pattern)
+                viewModel.onBuzzComplete()
+            }
+        })
+
         return binding.root
-
-    }
-
-    /**
-     * Resets the list of words and randomizes the order
-     */
-    private fun resetList() {
-        wordList = mutableListOf(
-                "queen",
-                "hospital",
-                "basketball",
-                "cat",
-                "change",
-                "snail",
-                "soup",
-                "calendar",
-                "sad",
-                "desk",
-                "guitar",
-                "home",
-                "railway",
-                "zebra",
-                "jelly",
-                "car",
-                "crow",
-                "trade",
-                "bag",
-                "roll",
-                "bubble"
-        )
-        wordList.shuffle()
     }
 
     /**
      * Called when the game is finished
      */
     private fun gameFinished() {
-        val action = GameFragmentDirections.actionGameToScore(score)
+        val currentScore = viewModel.score.value ?: 0
+        val action = GameFragmentDirections.actionGameToScore(currentScore)
         findNavController(this).navigate(action)
     }
 
-    /**
-     * Moves to the next word in the list
-     */
-    private fun nextWord() {
-        //Select and remove a word from the list
-        if (wordList.isEmpty()) {
-            gameFinished()
-        } else {
-            word = wordList.removeAt(0)
+    private fun buzz(pattern: LongArray) {
+        val buzzer : Vibrator = activity?.getSystemService(VIBRATOR_SERVICE) as Vibrator
+
+        buzzer?.let {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                buzzer.vibrate(VibrationEffect.createWaveform(pattern, -1))
+            } else {
+                //deprecated in API 26
+                buzzer.vibrate(pattern, -1)
+            }
         }
-        updateWordText()
-        updateScoreText()
     }
 
-    /** Methods for buttons presses **/
-
-    private fun onSkip() {
-        score--
-        nextWord()
-    }
-
-    private fun onCorrect() {
-        score++
-        nextWord()
-    }
-
-    /** Methods for updating the UI **/
-
-    private fun updateWordText() {
-        binding.wordText.text = word
-
-    }
-
-    private fun updateScoreText() {
-        binding.scoreText.text = score.toString()
-    }
 }
--- GuessIt/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ GuessIt/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -1,15 +1,157 @@
 package com.example.android.guesstheword.screens.game
 
-import android.util.Log
+import android.os.CountDownTimer
+import android.text.format.DateUtils
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 
+private val CORRECT_BUZZ_PATTERN = longArrayOf(100, 100, 100, 100, 100, 100)
+private val PANIC_BUZZ_PATTERN = longArrayOf(0, 200)
+private val GAME_OVER_BUZZ_PATTERN = longArrayOf(0, 2000)
+private val NO_BUZZ_PATTERN = longArrayOf(0)
+
+
 class GameViewModel : ViewModel() {
+
+
+    enum class BuzzType(val pattern: LongArray) {
+        CORRECT(CORRECT_BUZZ_PATTERN),
+        GAME_OVER(GAME_OVER_BUZZ_PATTERN),
+        COUNTDOWN_PANIC(PANIC_BUZZ_PATTERN),
+        NO_BUZZ(NO_BUZZ_PATTERN)
+    }
+
+    companion object {
+        const val DONE = 0L
+
+        const val ONE_SECOND = 1000L
+
+        const val COUNTDOWN_TIME = 20000L
+
+        const val COUNTDOWN_PANIC_SECONDS = 5L
+    }
+
+    private val timer : CountDownTimer
+
+    private val _currentTime = MutableLiveData<Long>()
+    private val currentTime : LiveData<Long>
+        get() = _currentTime
+
+    val currentTimeString = Transformations.map(currentTime, { time ->
+        DateUtils.formatElapsedTime(time)
+    })
+
+    // The current word
+    private val _word = MutableLiveData<String>()
+    val word : LiveData<String>
+        get() = _word
+
+    //buzzing stuff
+    private val _eventBuzz = MutableLiveData<BuzzType>()
+    val eventBuzz : LiveData<BuzzType>
+        get() = _eventBuzz
+
+    // The current score
+    private val _score = MutableLiveData<Int>()
+    val score : LiveData<Int>
+        get() = _score
+
+    // The list of words - the front of the list is the next word to guess
+    private lateinit var wordList: MutableList<String>
+
+    private val _eventGameFinish = MutableLiveData<Boolean>()
+    val eventGameFinish : LiveData<Boolean>
+        get() = _eventGameFinish
+
     init {
-        Log.i("GameViewModel", "GameViewModel created")
+        resetList()
+        nextWord()
+        _score.value = 0
+        _eventGameFinish.value = false
+
+        timer = object : CountDownTimer(COUNTDOWN_TIME, ONE_SECOND) {
+
+            override fun onTick(millisUntilFinished: Long) {
+                _currentTime.value = (millisUntilFinished / ONE_SECOND)
+                if (millisUntilFinished / ONE_SECOND <= COUNTDOWN_PANIC_SECONDS)
+                    _eventBuzz.value = BuzzType.COUNTDOWN_PANIC
+            }
+
+            override fun onFinish() {
+                _currentTime.value = DONE
+                _eventBuzz.value = BuzzType.GAME_OVER
+                _eventGameFinish.value = true
+            }
+        }
+
+        timer.start()
     }
 
     override fun onCleared() {
         super.onCleared()
-        Log.i("GameViewModel", "GameViewModel destroyed")
+        timer.cancel()
+    }
+
+    /**
+     * Resets the list of words and randomizes the order
+     */
+    private fun resetList() {
+        wordList = mutableListOf(
+                "queen",
+                "hospital",
+                "basketball",
+                "cat",
+                "change",
+                "snail",
+                "soup",
+                "calendar",
+                "sad",
+                "desk",
+                "guitar",
+                "home",
+                "railway",
+                "zebra",
+                "jelly",
+                "car",
+                "crow",
+                "trade",
+                "bag",
+                "roll",
+                "bubble"
+        )
+        wordList.shuffle()
+    }
+
+    /**
+     * Moves to the next word in the list
+     */
+    private fun nextWord() {
+        //Select and remove a word from the list
+        if (wordList.isEmpty()) {
+            resetList()
+        }
+        _word.value = wordList.removeAt(0)
+    }
+
+    /** Methods for buttons presses **/
+
+    fun onSkip() {
+        _score.value = (score.value)?.minus(1)
+        nextWord()
+    }
+
+    fun onCorrect() {
+        _score.value = (score.value)?.plus(1)
+        nextWord()
+    }
+
+    fun onGameFinishComplete() {
+        _eventGameFinish.value = false
+    }
+
+    fun onBuzzComplete() {
+        _eventBuzz.value = BuzzType.NO_BUZZ
     }
 }
